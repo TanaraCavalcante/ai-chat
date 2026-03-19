d6af263a71b87c40c89be12c8d8d1c23aa4920de
Tanara Cavalcante <tanara.cavalcante@w-tech.org>
2026-03-19 17:04:03 +0100

    fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ historico = []
 # ciclo principale del chatbot
 while True:
     # legge il messaggio dell'utente dal terminale
-    mensagem = input("Dimmi tutto: ")
+    mensagem = input("Tanara: ")
     
     # se l'utente scrive 'sair', termina il programma
     if mensagem.lower() == "sair":
